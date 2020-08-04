dd2888daf25d7df798ab63fcc238ba8561e76736
Becca <rbuechle@my365.bellevue.edu>
2020-08-04 16:18:36 -0600

    Week5

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -16,7 +16,7 @@ password: {type: String, required: true},
 email: {type: String, required: true}
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = module.exports = mongoose.model('User', userSchema);
 
 //user.save is used to add a new user in our database
 module.exports.add = (user, callback) => {
